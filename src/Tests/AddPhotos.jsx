f25d7dd5296490c4e1b8cdcd2ac778d573c33abc
praispranav <praispranav@gmail.com>
2021-05-19 17:47:54 +0530

    hello

--- src/Tests/AddPhotos.jsx
+++ src/Tests/AddPhotos.jsx
@@ -12,18 +12,5 @@ const AddPhotos = () => {
         console.log(event.target.value)
         setBackground({pranav:event.target.value})
     }
-    return (
-        <div>
-            <img src={logo} alt="none
-                       height: "2000"
-                    background:Background.pranav
-                }}
-            />
-            {Background.pranav  }
-            <input type="text" name="pranav"
-                onChange={(event)=> Back(event)} value={Background.pranav} />
-        </div>
-    );
-}
 
 export default AddPhotos;
