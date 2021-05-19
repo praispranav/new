4f016c652e761870d526c9e0b8d8b77ac64d1b2f
praispranav1 <84452771+praispranav1@users.noreply.github.com>
2021-05-19 17:53:35 +0530

    hello

--- src/Tests/AddPhotos.jsx
+++ src/Tests/AddPhotos.jsx
@@ -4,17 +4,6 @@ import logo from  "../logo.svg"
 const AddPhotos = () => {
     const [Background, setBackground] = useState({pranav:""});
     // const [backgroundText, setbackgroundText] = useState("")
-
-    useEffect(()=>{
-        setBackground({pranav:"red"})
-    },[])
-    const Back = (event)=>{
-        console.log(event.target.value)
-        setBackground({pranav:event.target.value})
-    }
-    return (
-        <div>
-            <img src={logo} alt="none
                        height: "2000"
                     background:Background.pranav
                 }}
